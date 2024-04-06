af6cad0b344b9e434294b7c12ed89f13177cb523
evolonation <1838940019@qq.com>
2024-04-06 20:27:13 +0800

    small fix

--- data_splitter.py
+++ data_splitter.py
@@ -7,8 +7,9 @@ def main():
     test_path = os.path.join('data', 'fmnist', 'data', 'test')
     dataset = read_data(train_path, test_path)
     users, _, train_data, test_data = dataset
+    os.mkdir('splitted_dataset')
     for i, u in enumerate(users):
-        with open(f'dataset/client_{i}.json', 'wt') as f:
+        with open(f'splitted_dataset/client_{i}.json', 'wt') as f:
             json.dump(test_data[u], f)
         
     

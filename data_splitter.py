aa4f13115d38c5ba48d3a0ef7b13b9c7947c34e2
evolonation <1838940019@qq.com>
2024-04-06 20:24:02 +0800

    脚本：用于将fmnist数据集拆分为属于不同客户端的多个json文件

---
+++ data_splitter.py
@@ -0,0 +1,16 @@
+import os
+import json
+from flearn.utils.model_utils import read_data
+
+def main():
+    train_path = os.path.join('data', 'fmnist', 'data', 'train')
+    test_path = os.path.join('data', 'fmnist', 'data', 'test')
+    dataset = read_data(train_path, test_path)
+    users, _, train_data, test_data = dataset
+    for i, u in enumerate(users):
+        with open(f'dataset/client_{i}.json', 'wt') as f:
+            json.dump(test_data[u], f)
+        
+    
+if __name__ == '__main__':
+    main()
